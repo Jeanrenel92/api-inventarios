cdc21a60d2e12ba59ce1c3cb79ace52181c0108a
Jeanrenel92 <jea.darius@duocuc.cl>
2026-05-28 23:24:06 -0400

    Initial commit

---
+++ src/main/java/cl/duoc/api_inventarios/ApiInventariosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.api_inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInventariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/api_inventarios/ApiInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.api_inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
