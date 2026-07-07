47106724047f1630d92eabbe0a9392700a8b08e4
B4yr0n-web <cerda.bayron@gmail.com>
2026-07-06 23:59:10 -0400

    ModelTest: Se relleno InventarioModelTest para verificar la correcta instanciación de las entidades y el funcionamiento de Lombok (Getters/Setters).
    ServiceTest: Se implementó InventarioServiceTests utilizando Mockito (@ExtendWith(MockitoExtension.class)) para aislar la base de datos. Se validaron los flujos de éxito y las reglas de negocio (ej. validación de fabricante duplicado al ingresar un componente).
    
    ControllerTest: Se construyó inventarioControllerTest usando @WebMvcTest y MockMvc para simular peticiones HTTP. Se verificaron las respuestas correctas de la API (HTTP 200 OK y HTTP 404 Not Found) aislando la lógica de negocio con simulación de dependencias.

--- src/test/java/cl/duoc/api_inventarios/Controller/inventarioControllerTest.java
+++ src/test/java/cl/duoc/api_inventarios/Controller/inventarioControllerTest.java
@@ -1,4 +1,52 @@
 package cl.duoc.api_inventarios.Controller;
 
+import cl.duoc.api_inventarios.Model.Componente;
+import cl.duoc.api_inventarios.Service.ComponenteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+// Carga solo la capa Web (Controlador) para mas rapidez
+@WebMvcTest(ComponenteController.class)
 public class inventarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ComponenteService componenteService;
+
+    @Test
+    void buscarPorId_DeberiaRetornar200() throws Exception {
+        Componente componente = new Componente();
+        componente.setId(1L);
+        componente.setNombre("Teclado Mecánico");
+
+        when(componenteService.buscarPorId(1L)).thenReturn(Optional.of(componente));
+
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/inventarios/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()) // Esperamos HTTP 200 ok
+                .andExpect(jsonPath("$.nombre").value("Teclado Mecánico")); // Revisamos el JSON
+    }
+
+    @Test
+    void buscarPorId_NoEncontrado_DeberiaRetornar404() throws Exception {
+        when(componenteService.buscarPorId(99L)).thenReturn(Optional.empty());
+
+        // Act & Assert ejecutar y verificar
+        mockMvc.perform(get("/api/v1/inventarios/99")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound()); // Esperamos HTTP 404 NotFound
+    }
 }
