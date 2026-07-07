c6bf341e6ccaca3675e29498e268ea7793f78a0b
Jeanrenel92 <jea.darius@duocuc.cl>
2026-07-07 19:39:37 -0400

    Actualizacion: Creacion del repositorio para la clase Auditoria con metodo para filtra entre 2 fechas

--- src/main/java/cl/duoc/api_inventarios/Repository/AuditoriaRepository.java
+++ src/main/java/cl/duoc/api_inventarios/Repository/AuditoriaRepository.java
@@ -1,8 +1,13 @@
 package cl.duoc.api_inventarios.Repository;
-/*
+
 import cl.duoc.api_inventarios.Model.Auditoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
+
+    List<Auditoria> findByFechaBetween(LocalDate inicio, LocalDate fin);
 }
- */
